5180b5481a85a18f012a900dd699d1754a688a26
joealsaaidah <alsaaidahyousef@gmail.com>
2022-02-15 19:20:25 +0800

    chakra ui + react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import ChatPage from "./ChatPage";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/chats' element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
